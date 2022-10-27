9b9c0e2ac233b8f98424744ca7452f90862a41a2
Merl Creps <merl.creps@steeldynamics.com>
2022-10-27 16:11:02 -0400

    Added Missing File

---
+++ ShimApi/Models/SalesShipmentModels/UnloadWeightsCreate.cs
@@ -0,0 +1,8 @@
+﻿namespace ShimApi.Models.SalesShipmentModels
+{
+    public class UnloadWeightsCreate : ServiceAware
+    {
+        public string DataEnvironmentName { get; init; } = string.Empty;
+        public SalesShipmentUnloadWeights UnloadWeights { get; set; } = new();
+    }
+}
